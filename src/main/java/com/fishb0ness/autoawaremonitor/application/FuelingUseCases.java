1c54816d67ad37ddca9e1bff0a7e7a7532cabbc4
WDP <asdfghjewr@adsfgfnn435.com>
2024-03-31 00:16:35 +0100

    Added fueling use cases

---
+++ src/main/java/com/fishb0ness/autoawaremonitor/application/FuelingUseCases.java
@@ -0,0 +1,22 @@
+package com.fishb0ness.autoawaremonitor.application;
+
+import com.fishb0ness.autoawaremonitor.domain.fueling.Fueling;
+import com.fishb0ness.autoawaremonitor.domain.fueling.FuelingId;
+import com.fishb0ness.autoawaremonitor.domain.vehicle.VehicleModelId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FuelingUseCases {
+
+    //
+    Fueling createFueling(Fueling fueling);
+
+    Optional<Fueling> getFuelingById(FuelingId fuelingId);
+
+    List<Fueling> getAllFuelingByVehicleId(FuelingId vehicleId);
+
+    Optional<Fueling> updateFuelingInfo(Fueling newfueling);
+
+    long deleteFueling(FuelingId fuelingId);
+}
